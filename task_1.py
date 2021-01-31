b4d93983ab15b6c0033a297220cb34a36653f0cf
igor91m <igor91m@yandex.ru>
2021-02-01 02:21:11 +0300

    add changes

--- task_1.py
+++ task_1.py
@@ -10,3 +10,29 @@
 и завершать скрипт.
 '''
 
+import time
+
+
+class TrafficLight:
+    __color = 'Красный' # приватный __атрибут класса
+
+    def running(self): # собственный метод класса
+        print('Светофор работает')
+
+        self.__color = 'Красный'
+        print(f'Установлен цвет: {self.__color}')
+        time.sleep(7)
+
+        self.__color = 'Желтый'
+        print(f'Установлен цвет: {self.__color}')
+        time.sleep(2)
+
+        self.__color = 'Зеленый'
+        print(f'Установлен цвет: {self.__color}')
+        time.sleep(7) # время для равного значения с красным цветом светофора
+
+        while True:
+            self.running()
+
+traff_light = TrafficLight()
+print(traff_light.running())
