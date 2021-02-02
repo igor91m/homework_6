61fbba95f8dd8d0eb767dbe04dbcca7bd2c004f0
igor91m <igor91m@yandex.ru>
2021-02-02 14:05:23 +0300

    add changes

--- task_2.py
+++ task_2.py
@@ -8,3 +8,18 @@
 Например: 20м * 5000м * 25кг * 5см = 12500 т
 '''
 
+class Road:
+
+    def __init__(self, length, width):
+        self._length = length
+        self._width = width
+        self.weight = 25
+        self.height = 5
+
+    def asphalt_mass(self):
+        asphalt_mass = self._length * self._width * self.weight * self.height / 1000
+        print(f'Для покрытия дорожного полотна нужно {round(asphalt_mass, 2)}т. массы асфальта')
+
+
+r = Road(5000, 20)
+r.asphalt_mass()
