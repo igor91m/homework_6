60505235b2e82cf6328b6431062e89359d6185d0
igor91m <igor91m@yandex.ru>
2021-02-06 14:54:20 +0300

    add changes

--- task_5.py
+++ task_5.py
@@ -7,3 +7,29 @@
 Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
 '''
 
+class Stationery:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        return print('Запуск отрисовки')
+
+class Pen(Stationery):
+    def draw(self):
+        return print(f'Запуск отрисовки {self.title}')
+
+class Pencil(Stationery):
+    def draw(self):
+        return print(f'Запуск отрисовки {self.title}')
+
+class Handle(Stationery):
+    def draw(self):
+        return print(f'Запуск отрисовки {self.title}')
+
+pen = Pen('ручкой')
+print(pen.draw())
+pencil = Pencil('карандашом')
+print(pencil.draw())
+handle = Handle('маркером')
+print(handle.draw())
+
